82efc785966bfa2187126476a59a6f65b186d7ae
Anurag Uday Khanapurkar <x24214787@student.ncirl.ie>
2026-04-16 03:23:22 +0100

    update2.7

--- fogedgedashboard/urls.py
+++ fogedgedashboard/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('dashboard/', views.dashboard_view, name='dashboard'),
     path('toggle-override/', views.toggle_override, name='toggle_override'),
     path('get-dashboard-data/', views.api_get_dashboard_data, name='get_dashboard_data'),
+    path('get-session-data/', views.api_get_session_data, name='get_session_data'),
 ]
